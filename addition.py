e92d036094b11a3708c8c1cf8ff3f62b63ca9285
karthikalagappan29 <karthikalagappan02@gmail.com>
2024-01-13 23:44:34 +0530

    second commit

---
+++ addition.py
@@ -0,0 +1,9 @@
+a  = input('Enter the first value ::')
+a1 = input('Enter the 2nd value ::')
+
+if a>a1:
+    print( str(a) +' is greater than '+ str(a1))
+elif a<a1:
+    print(str(a) + ' is lesser than ' + str(a1))
+else:
+    print(str(a) + ' is equal to ' + str(a1))
